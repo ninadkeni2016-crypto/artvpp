447ae6d24f6919a2b5ea469888d13165ab7c60b1
Ninad Santosh Keni <ninadkeni2016@gmail.com>
2026-02-09 00:26:21 +0530

    added api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { User, Product, Order, Service, Course } from '../app/types';
 
 // API Base URL
-const API_URL = 'http://localhost:4000';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create Axios instance
 const api: AxiosInstance = axios.create({
